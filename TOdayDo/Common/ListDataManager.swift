2d1e56c4f083c1bbb0ead5aeeab05466057faeb6
Jiseok97 <wltjr1426@naver.com>
2024-01-04 19:15:02 +0900

    [Feat] ListDataManager ListModel 타입 변환

--- TOdayDo/Common/ListDataManager.swift
+++ TOdayDo/Common/ListDataManager.swift
@@ -9,27 +9,27 @@ import Foundation
 
 final class ListDataManager {
     public func saveData(
-        data: ListModel
+        data: [ListModel]
     ) {
         do {
             let data = try JSONEncoder().encode(data)
-            UserDefaults.standard.set(data, forKey: "lsitData")
+            UserDefaults.standard.set(data, forKey: "listData")
             print("DEBUG: ListDataManager saveData() → 데이터 저장에 성공했습니다.")
         } catch {
             print("DEBUG: ListDataManager saveData() 에러, \(error.localizedDescription)")
         }
     }
     
-    public func loadData() -> ListModel {
+    public func loadData() -> [ListModel] {
         if let data = UserDefaults.standard.data(forKey: "listData") {
             do {
                 print("DEBUG: ListDataManager loadData() → 데이터 로드에 성공했습니다.")
-                return try JSONDecoder().decode(ListModel.self, from: data)
+                return try [JSONDecoder().decode(ListModel.self, from: data)]
             } catch {
                 print("DEBUG: ListDataManager loadData() 에러, \(error.localizedDescription)")
             }
         }
         print("DEBUG: ListDataManager loadData() → 데이터 로드에 성공했습니다.\n Empty data")
-        return ListModel(title: "", startTime: Date(), endTime: Date())
+        return [ListModel(title: "", startTime: Date(), endTime: Date())]
     }
 }
